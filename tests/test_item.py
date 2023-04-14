09bd0e37c0709c758c4965bb7fdfd6bfd0557092
Aleksey Krivov <adkrivov@gmail.com>
2023-04-14 08:43:05 +0300

    Добавлен код для класса и тестов

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,15 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+@pytest.fixture()
+def mouse():
+    return Item("Мышь", 300.5, 1)
+def test_name(mouse):
+    assert mouse.name == 'Мышь'
+
+def test_price(mouse):
+    assert mouse.price != 300
+
+def test_quantity(mouse):
+    assert mouse.quantity == 1
